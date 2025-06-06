1634b314f2981b5cf74fa7e5b3378d65190367df
Isaac923 <kakkoelsabio923@gmail.com>
2025-06-05 23:43:50 -0400

    css-notas

--- EDUVIA/EDUVIA/settings.py
+++ EDUVIA/EDUVIA/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'EDUVIA.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BDEduvia',
+        'NAME': 'DBEduvia',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
